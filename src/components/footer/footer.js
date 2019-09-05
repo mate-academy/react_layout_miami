a87eaf2738b431ed6bf550e2def93909ceb86c96
Aleks <alex.andrutskii@gmail.com>
2019-09-06 00:21:11 +0300

    Divided the site into lagre components

---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+  return (
+      <footer className="footer">
+        <div className="footer__logo">
+          <img className="footer__logo-image" src="/images/logo.svg" alt="Miami logo" />
+        </div>
+        <div className="footer__copy">© 2013 Miami.</div>
+      </footer>
+  );
+}
+
+export default Footer;
