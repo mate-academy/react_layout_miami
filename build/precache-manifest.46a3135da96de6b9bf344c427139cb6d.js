0637df4d5b1449517e913aea129a98d0aa8b2cff
VladyslavSak <iblackvour@gmail.com>
2019-09-05 18:54:11 +0300

    Some fix

---
+++ build/precache-manifest.46a3135da96de6b9bf344c427139cb6d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c948e6d3a6469313b6d7e35407b94c37",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5ba41a492066744b3f7",
+    "url": "./static/css/main.dc9d7ca7.chunk.css"
+  },
+  {
+    "revision": "01c451dc922cb85b4ec5",
+    "url": "./static/js/2.f7982c1a.chunk.js"
+  },
+  {
+    "revision": "a5ba41a492066744b3f7",
+    "url": "./static/js/main.f0d6c1f4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "58ac6272eb60de5ab71b700b2af824bb",
+    "url": "./static/media/apart1.58ac6272.jpg"
+  },
+  {
+    "revision": "94688e9b12225980b08e6a30563d74a6",
+    "url": "./static/media/apart2.94688e9b.jpeg"
+  },
+  {
+    "revision": "d1655395ec4331f18c78d2011b7285d3",
+    "url": "./static/media/apart3.d1655395.jpg"
+  },
+  {
+    "revision": "68a13768bd1c4e926fcfa569252a9354",
+    "url": "./static/media/apart4.68a13768.jpg"
+  },
+  {
+    "revision": "5baa8143dc410c6a08b2f49631e1dcec",
+    "url": "./static/media/apart5.5baa8143.jpg"
+  },
+  {
+    "revision": "d7e2d56679fd29dc6ec3fcab376c9b27",
+    "url": "./static/media/apart6.d7e2d566.jpg"
+  },
+  {
+    "revision": "4f75d12689b3f338bb51e7d934c95638",
+    "url": "./static/media/bg_header.4f75d126.jpg"
+  },
+  {
+    "revision": "70839551842c7e2b12c7551060194abf",
+    "url": "./static/media/footer.70839551.jpg"
+  },
+  {
+    "revision": "a4628b62063e42dd8747d6b9656212c3",
+    "url": "./static/media/part_of_title.a4628b62.svg"
+  },
+  {
+    "revision": "5acd85d3cd3bcc42aa1bc44af7f4288a",
+    "url": "./static/media/person1.5acd85d3.png"
+  },
+  {
+    "revision": "00c01726c3e4bd61d4a53ea5727aa310",
+    "url": "./static/media/person2.00c01726.png"
+  },
+  {
+    "revision": "b9754f3124dca5d25e4fd6798f9f9861",
+    "url": "./static/media/person3.b9754f31.png"
+  },
+  {
+    "revision": "13242e851d8eb9e421411534c9fff3cc",
+    "url": "./static/media/quote.13242e85.svg"
+  }
+]);
