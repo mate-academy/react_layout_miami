0d41738f39e68f5bec8f4f3f34f2659548309228
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-09-05 21:52:55 +0300

    Component: SectionBest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.scss';
-import Hero from './components/hero';
+import Hero from './components/Hero';
+import SectionBest from './components/SectionBest';
 
 function App() {
   return (
-    <Hero />
+    <div className="wrapper">
+      <Hero />
+      <SectionBest />
+    </div>
   );
 }
 
