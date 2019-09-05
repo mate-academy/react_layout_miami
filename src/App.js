4c6ca29409b76a580a195b77b826b2fa3c2b3ed1
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-09-05 21:26:30 +0300

    assetes; Component: Hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.scss';
+import Hero from './components/hero';
 
 function App() {
   return (
-    <div className="page">
-      Hello React!
-    </div>
+    <Hero />
   );
 }
 
