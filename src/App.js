f27c2d2e7b2ec481dccbee143b7940d1cc8a488b
VladyslavSak <vladyslav.sak@gmail.com>
2019-09-05 12:32:58 +0300

    Logical block dividing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="page">
-      Hello React!
-    </div>
+    <Header />
   );
 }
 
