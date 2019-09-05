43f0edad3dc651380f8d2b68e50bd073cf4151f9
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-09-05 12:33:08 +0300

    firts

---
+++ build/precache-manifest.97c114c8d8d981d224f426861c5790eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a7f6919daa00718804b45800762338",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49b03f2b8e70d14176bb",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "49b03f2b8e70d14176bb",
+    "url": "./static/js/main.6c4be4d8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
