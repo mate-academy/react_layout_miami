e0ce0e16485ad51af229b52b7e6a9d08b1342a54
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-09-06 07:47:17 +0300

    Assets; Components

---
+++ build/precache-manifest.543689a629295b65d28796e909f1d2fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed07ecc0f0ab921acbc1ce7709df40ab",
+    "url": "/react_layout_miami/index.html"
+  },
+  {
+    "revision": "05944359d730482ef3b8",
+    "url": "/react_layout_miami/static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/react_layout_miami/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "05944359d730482ef3b8",
+    "url": "/react_layout_miami/static/js/main.69112e32.chunk.js"
+  },
+  {
+    "revision": "4dcf5508f0a9bed7a82c",
+    "url": "/react_layout_miami/static/js/runtime~main.59dfb4f6.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
+import Wrapper from './components/Wrapper';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="page">
-      Hello ViCtor!
+    <div>
+      <Wrapper />
+      <Footer />
     </div>
   );
 }
