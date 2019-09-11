b8c879c6782442196d4a19c065bd8bc4e33c4192
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-09-11 13:37:02 +0300

    changed package.json

---
+++ build/precache-manifest.06104aa587d799270145b03dde692da3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d85e18069e55fe37fdc64a8274886c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a4f5fa4b4ce403ffca0",
+    "url": "/static/css/main.235e7c1a.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "8a4f5fa4b4ce403ffca0",
+    "url": "/static/js/main.355cd408.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "41a73d7d4b28db3cd2d21cf384153d58",
+    "url": "/static/media/bg.41a73d7d.png"
+  },
+  {
+    "revision": "03963ebbddd7eaa95d514fb8ea537e04",
+    "url": "/static/media/footer.03963ebb.png"
+  },
+  {
+    "revision": "13242e851d8eb9e421411534c9fff3cc",
+    "url": "/static/media/quote.13242e85.svg"
+  }
+]);
